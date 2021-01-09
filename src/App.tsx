4c41807749e39eda015fddfa3565f60fe8a58984
hoanghuychh <hoanghuychh.dev@gmail.com>
2021-01-09 22:32:40 +0700

    modify icon

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import * as React from 'react';
 import {useTranslation} from 'react-i18next';
 import RNBootSplash from 'react-native-bootsplash';
 import Icon from 'react-native-easy-icon';
+
+import * as eva from '@eva-design/eva';
+import {ApplicationProvider, Layout, Text} from '@ui-kitten/components';
+
 import Home from 'src/containers/Home';
 import Settings from 'src/containers/Settings';
 import {sleep} from './utils/async';
@@ -22,44 +26,46 @@ const App = () => {
 
   React.useEffect(() => {
     init().finally(() => {
-      RNBootSplash.hide({duration: 250}); // fade animation
+      RNBootSplash.hide({duration: 150}); // fade animation
     });
   }, []);
 
   const {t} = useTranslation();
   return (
-    <Tab.Navigator initialRouteName="home">
-      <Tab.Screen
-        name="home"
-        component={Home}
-        options={{
-          tabBarLabel: t('home'),
-          tabBarIcon: ({focused, color, size}) => (
-            <Icon
-              name={focused ? 'home' : 'home-outline'}
-              type="material-community"
-              size={size}
-              color={color}
-            />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="settings"
-        component={Settings}
-        options={{
-          tabBarLabel: t('settings'),
-          tabBarIcon: ({focused, color, size}) => (
-            <Icon
-              name={focused ? 'cog' : 'cog-outline'}
-              type="material-community"
-              size={size}
-              color={color}
-            />
-          ),
-        }}
-      />
-    </Tab.Navigator>
+    <ApplicationProvider {...eva} theme={eva.light}>
+      <Tab.Navigator initialRouteName="home">
+        <Tab.Screen
+          name="home"
+          component={Home}
+          options={{
+            tabBarLabel: t('home'),
+            tabBarIcon: ({focused, color, size}) => (
+              <Icon
+                name={focused ? 'home' : 'home-outline'}
+                type="material-community"
+                size={size}
+                color={color}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="settings"
+          component={Settings}
+          options={{
+            tabBarLabel: t('settings'),
+            tabBarIcon: ({focused, color, size}) => (
+              <Icon
+                name={focused ? 'cog' : 'cog-outline'}
+                type="material-community"
+                size={size}
+                color={color}
+              />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </ApplicationProvider>
   );
 };
 
